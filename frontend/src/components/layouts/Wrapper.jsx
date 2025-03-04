9f5c6d24ae1f4171c1e097e8c99c32192bad0053
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-05 04:15:37 +0700

    REMOVED: Padding top

--- frontend/src/components/layouts/Wrapper.jsx
+++ frontend/src/components/layouts/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Wrapper({ children }) {
   return (
-    <div className='pt-10 px-8 w-full min-h-screen'>
+    <div className='px-8 w-full min-h-screen'>
         {children}
     </div>
   )
