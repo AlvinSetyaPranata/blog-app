e7ab6441e9ac6819383eb3fb972198352044a35d
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-05 08:59:43 +0700

    UPDATE: Migrating to using entity manager

--- backend/src/Controller/PostController.php
+++ backend/src/Controller/PostController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\User;
 use App\Repository\PostRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
@@ -20,7 +22,7 @@ final class PostController extends AbstractController
     }
 
     #[Route('/api/post', name: 'app_post', methods: ["GET"])]
-    public function index(PostRepository $repository): JsonResponse
+    public function index(EntityManagerInterface $em): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
@@ -29,18 +31,25 @@ final class PostController extends AbstractController
     }
 
     #[Route('/api/post', name: 'app_add_post', methods: ["POST"])]
-    public function create(EntityManagerInterface $em): JsonResponse
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
     {
 
-        $data = json_decode($this->requestStack->getCurrentRequest(), true);
+        $data = $request->request->all();
 
-        if (!isset($data["title"], $data["author_id"])) {
+        if (!isset($data["title"], $data['content'], $data["author_id"])) {
             return new JsonResponse(['messege' => 'Invalid Request'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $user = $em->getRepository(User::class)->find($data["author_id"]);
+
+        if (!$user) {
+            return new JsonResponse(['messege' => 'Failed to find a user that requested'], JsonResponse::HTTP_NOT_FOUND);
         }
 
         $post = new Post();
         $post->setTitle($data["title"]);
-        $post->setAuthorId($data["author"]);
+        $post->setContent($data["content"]);
+        $post->setAuthorId($user);
 
         $em->persist($post);
         $em->flush();
