6ca003ebefaae0c1f81605c7d9ca13b755958c74
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-06 07:56:44 +0700

    UPDATE: Added border radius

--- frontend/src/components/CardOverlay.jsx
+++ frontend/src/components/CardOverlay.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CardOverlay({ title, desc, src }) {
   return (
     <div className='rounded-md group relative'>
-        <div className='absolute left-0 top-0 size-full bg-black opacity-0 group-hover:opacity-75 group-hover:transition-opacity group-hover:ease-in group-hover:duration-200 duration-200 ease-out flex flex-col justify-end p-8'>
+        <div className='absolute left-0 top-0 size-full bg-black opacity-0 group-hover:opacity-75 group-hover:transition-opacity group-hover:ease-in group-hover:duration-200 duration-200 ease-out flex flex-col justify-end p-8 rounded-md'>
             <h1 className='text-white text-lg font-semibold'>{title}</h1>
             <h3 className='text-white text-sm'>{desc}</h3>
         </div>
