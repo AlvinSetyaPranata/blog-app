34c9600d32dc15e17a50440a30dd719939e96d41
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-07 15:37:09 +0700

    UPDATE: Using google oauth provide

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId='180825512957-02c5lgnajedu8ltiovg6lga2j5cscemj.apps.googleusercontent.com'>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
