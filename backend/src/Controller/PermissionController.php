adb55f9b4cfaee44f4a428a8b1c91f27ad786076
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-10 08:38:09 +0700

    UPDATE: Added Create, Read, Update, and Delete

--- backend/src/Controller/PermissionController.php
+++ backend/src/Controller/PermissionController.php
@@ -2,18 +2,79 @@
 
 namespace App\Controller;
 
+use App\Entity\Permission;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 final class PermissionController extends AbstractController
 {
-    #[Route('/permission', name: 'app_permission')]
-    public function index(): JsonResponse
+    #[Route('/api/permissions', name: 'get_permissions', methods: ['GET'])]
+    public function index(EntityManagerInterface $em, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PermissionController.php',
-        ]);
+        $query = $em->getRepository(Permission::class)->findAll();
+        $data = $serializer->serialize($query, "json", ['groups' => 'permission:read']);
+
+        return new JsonResponse(["messege" => "List of Permissions", "data" => json_decode($data, true)]);
     }
+
+    #[Route('/api/permission', name: 'add_permission', methods: ['POST'])]   
+    public function create(EntityManagerInterface $em, Request $request): JsonResponse
+    {
+        $reqData = json_decode($request->getContent(), true);
+
+        if (!isset($reqData["name"])) {
+            return new JsonResponse(["messege" => "Invalid Request!"], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $permission = new Permission();
+
+        $permission->setName($reqData["name"]);
+
+        $em->persist($permission);
+        $em->flush();
+
+        return new JsonResponse(["messege" => "Successfully added new permission"], JsonResponse::HTTP_CREATED);
+    } 
+
+    #[Route('/api/permission/{id}', name: 'update_permission', methods: ['PUT'])]
+    public function update(int $id, EntityManagerInterface $em, Request $request): JsonResponse
+    {
+        $permission = $em->getRepository(Permission::class)->find($id);
+
+        if (!$permission) {
+            return new JsonResponse(["messege" => "Permission with the given id, is not found!"], JsonResponse::HTTP_CONFLICT);
+        }
+
+        $reqData = json_decode($request->getContent(), true);
+
+        if (isset($reqData["name"])) {
+            $permission->setName($reqData["name"]);
+        }
+
+        $em->persist($permission);
+        $em->flush();
+
+        return new JsonResponse(JsonResponse::HTTP_NO_CONTENT);
+    }   
+
+    #[Route('/api/permission/{id}', name: 'delete_permission', methods: ['DELETE'])]
+    public function delete(int $id, EntityManagerInterface $em): JsonResponse 
+    {
+        $permission = $em->getRepository(Permission::class)->find($id);
+
+        if (!$permission) {
+            return new JsonResponse(["messege" => "Permission with the given id, is not found!"], JsonResponse::HTTP_CONFLICT);
+        }
+        
+        $em->remove($permission);
+        $em->flush();
+
+        return new JsonResponse(JsonResponse::HTTP_NO_CONTENT);
+    }
+
+
 }
---
+++ backend/src/Entity/Permission.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PermissionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: PermissionRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Permission
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['role:read', 'permission:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100, unique: true)]
+    #[Groups(['role:read', 'permission:read'])]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Role>
+     */
+    #[ORM\ManyToMany(targetEntity: Role::class, mappedBy: 'permissions')]
+    private Collection $roles;
+
+    public function __construct()
+    {
+        $this->roles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Role>
+     */
+    public function getRoles(): Collection
+    {
+        return $this->roles;
+    }
+
+    public function addRole(Role $role): static
+    {
+        if (!$this->roles->contains($role)) {
+            $this->roles->add($role);
+            $role->addPermission($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRole(Role $role): static
+    {
+        if ($this->roles->removeElement($role)) {
+            $role->removePermission($this);
+        }
+
+        return $this;
+    }
+}
