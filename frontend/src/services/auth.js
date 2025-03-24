1275091fef0d8fc50a358b2ab9d508d13193e4e4
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-24 11:07:50 +0700

    ADD: Added login service

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,22 @@
+export async function NormalLogin(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+
+    const data = {}
+
+    // console.log(formData)
+    formData.forEach((value, name) => {
+        data[name] = value
+    })
+
+
+    const res = await fetch(`${import.meta.env.VITE_BASE_API_URL}/auth/login`, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+
+    if (!res.ok) return [false, null]
+
+    return [true, await res.json]
+
+}
