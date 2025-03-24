1fb390278d3c3c27a3c6032e4d3a4d44a6e3c5c7
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-24 13:14:43 +0700

    UPDATE: Implement user profile based on data

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { easeIn, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import FloatingLogin from "./FloatingLogin";
 import { useNavigate } from "react-router-dom";
+import useAuthStore from "../store/auth";
 
 export default function Navbar() {
   const [showPopup, setShowPopup] = useState(false);
   const [showForm, setShowForm] = useState(false)
+
+  const { user, token } = useAuthStore()
 
   const navigation = useNavigate()
 
@@ -88,28 +91,15 @@ export default function Navbar() {
           </div>
         </motion.div>
           {/* popup */}
-        {false ? (
+        {token ? (
           <>
             <a
               onMouseEnter={() => setShowPopup(true)}
               onMouseLeave={() => setShowPopup(false)}
               href="#"
-              className={`rounded-full ${!credential ? "bg-gray-600 animate-pulse" : ""} hover:cursor-pointer bg-red-500`}
+              className={`rounded-full size-[40px] ${!user ? "bg-gray-600 animate-pulse" : ""} hover:cursor-pointer bg-red-500`}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-                />
-              </svg>
+              <img src={user.avatar} alt="user-avatar" className="size-full aspect-video" />
             </a>
 
             {/* popup */}
