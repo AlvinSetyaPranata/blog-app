c2e3a55d820244e2e7119974c79a74fd6d6d4968
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-24 13:16:07 +0700

    UPDATE: Using SessionStorage as storage for auth store

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,8 +1,34 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
-const useAuthStore = create(set => ({
- setToken: (token) => set({token: token}),
- token: ""   
-}))
+const useAuthStore = create()(persist(
+    set => ({
+        setToken: (token) => set({ token: token }),
+        setUser: (userData) => set({ user: userData }),
+        user: {
+            id: "",
+            email: "",
+            name: "",
+            age: "",
+            gender: "",
+            avatar: "",
+            role: {
+                id: "",
+                name: "",
+                date_created: ""
+            },
+            date_registered: ""
+        },
+        token: ""
+    }),
+    {
+        name: "auth",
+        storage: {
+          getItem: (key) => JSON.parse(sessionStorage.getItem(key)),
+          setItem: (key, value) => sessionStorage.setItem(key, JSON.stringify(value)),
+          removeItem: (key) => sessionStorage.removeItem(key),
+        },
+      }
+))
 
 export default useAuthStore
