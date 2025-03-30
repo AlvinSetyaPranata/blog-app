39fab8bee469b47938d077888c7396ee477499cf
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-30 11:18:30 +0700

    UPDATE: Add new page

---
+++ frontend/src/pages/Accounts/Blogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardWithOption from '../../components/Cards/CardWithOption'
+
+export default function Blogs() {
+  return (
+    <div className='pl-12 grid grid-cols-3 gap-x-6'>
+      <CardWithOption title="Cinta di langit dubai" desc="21 Feb 2024" imageURL="/woman-eyes.jpg" />
+      <CardWithOption title="Cinta di langit dubai" desc="21 Feb 2024" imageURL="/woman-eyes.jpg" />
+      <CardWithOption title="Cinta di langit dubai" desc="21 Feb 2024" imageURL="/woman-eyes.jpg" />
+    </div>
+  )
+}
