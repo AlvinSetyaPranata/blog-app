4dd8e8ffbf69a434712ca6cd9b5b86b801691f83
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-03-30 11:13:24 +0700

    UPDATE: Re-structure routers

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -4,14 +4,21 @@ import RootLayout from '../components/layouts/RootLayout'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+import AccountLayout from '../components/layouts/Account'
+import Blogs from '../pages/Accounts/Blogs'
 
 export default function Routers() {
   return (
     <BrowserRouter>
         <Routes>
+          
             <Route path='/' element={<RootLayout />}>
               <Route path='/' element={<Home />} />
+              <Route path='/account' element={<AccountLayout/>}>
+                <Route path='/account/dashboard' element={<Blogs />} />
+              </Route>
             </Route>
+
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
         </Routes>
